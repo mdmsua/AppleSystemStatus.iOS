e1df8a2d0f91a33b360b282e3b38b347de3e5dfc
Dmytro Morozov <mdmsua@icloud.com>
2020-12-27 11:58:21 +0100

    Initial Commit

---
+++ AppleSystemStatus/AppleSystemStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSystemStatusApp.swift
+//  AppleSystemStatus
+//
+//  Created by Dmytro Morozov on 27.12.20.
+//
+
+import SwiftUI
+
+@main
+struct AppleSystemStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
